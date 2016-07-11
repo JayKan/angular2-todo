0a312dedc315b2b7d389b092842e1c2b989720d8
Jay Kan <leicasper@gmail.com>
2016-07-11 11:44:57 -0700

    chore: enable production mode for faster rendering

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,6 +1,10 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
 import { TodoApp } from './components/todo/todo';
 import { TodoStore } from './components/todo/todo.service';
+
+// enable prod for faster renders
+enableProdMode();
 
 bootstrap(TodoApp, [TodoStore])
   .then(success => console.log('Bootstrap success'))
